de6b8bda3f9b65a08ebce3655b5ff912069ba345
animay7860 <prakashanimay03@gmail.com>
2026-08-03 00:07:53 +0530

    Initial Commit

---
+++ app.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class app {
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World");
+    }
+}
